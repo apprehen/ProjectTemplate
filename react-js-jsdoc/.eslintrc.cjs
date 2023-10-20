3292a8d76170574ee819f16a411531c38dbae248
apprehen <3514392356@qq.com>
2023-10-20 17:14:07 +0800

    init(react): add the react-js init project

---
+++ react-js-jsdoc/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'eslint-config-prettier'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'react'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'never'],
+    'prettier/prettier': 'error',
+    'react/jsx-no-target-blank': 'off'
+  }
+}
